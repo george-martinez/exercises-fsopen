c6f1ebea3527b10fc26b53210655d0ea58eff276
George Martinez <gemartinez@fi.uba.ar>
2022-10-14 03:34:02 -0300

    exercises blog 4.3 to 4.7 done

--- part4/controllers/blogsController.js
+++ part4/controllers/blogsController.js
@@ -3,21 +3,21 @@ const Blog = require('../models/blogModel')
 const blogRouter = require('express').Router()
 
 blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
 
 blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
 module.exports = blogRouter
--- part4/models/blogModel.js
+++ part4/models/blogModel.js
@@ -5,8 +5,8 @@ const blogSchema = new mongoose.Schema({
     author: String,
     url: String,
     likes: Number
-  })
+})
   
-  const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema)
 
-  module.exports = Blog
+module.exports = Blog
