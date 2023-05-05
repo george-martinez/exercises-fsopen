ffbcb39e4015b44cd8affeb217f9287b2b9642e6
George Martinez <gemartinez@fi.uba.ar>
2023-05-05 04:48:44 -0300

    exercises routed-anecdotes 7.4 to 7.6 done

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -56,26 +57,34 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
   const navigate = useNavigate()
+  const [ content, contentReset ] = useField('text')
+  const [ author, authorReset ] = useField('text')
+  const [ info, infoReset ] = useField('text')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
     navigate('/')
-    props.setNotification(`a new anecdote ${content} was created!`)
+
+    props.setNotification(`a new anecdote ${content.value} was created!`)
     setTimeout(() => {
       props.setNotification(null)
     }, 5000)
+  }
+  
+  const handleReset = (e) => {
+    e.preventDefault()
+    contentReset()
+    authorReset()
+    infoReset()
   }
 
   return (
@@ -84,17 +93,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info}/>
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = (event) => {
+    setValue('')
+  }
+
+  const field = [
+    {
+      type,
+      value,
+      onChange,
+    },
+    reset
+  ]
+
+  return field
+}
